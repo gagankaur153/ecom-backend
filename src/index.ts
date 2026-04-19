9b6054742250dc59eda0e6e9ab234a805b42cae6
gagankaur153 <gagankaur672000@gmail.com>
2026-04-19 11:42:40 +0530

    dns added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 import express, { Application } from 'express'
+import dns from "node:dns";
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieparser from 'cookie-parser'
